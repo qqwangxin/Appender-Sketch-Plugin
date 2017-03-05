08e208eb25e1de8e93217ee6ba300252a1f6ec72
ESW1234 <xin.wang@xinxang-ltm2.internal.salesforce.com>
2017-03-04 23:48:30 -0800

    Commit translation.

--- Appender.sketchplugin/Contents/Sketch/append_text.js
+++ Appender.sketchplugin/Contents/Sketch/append_text.js
@@ -1,4 +1,6 @@
 var nonTextSelected = 0
+var TRANSLATE_URL = "https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20170305T053617Z.246e0c7605e30fc1.f58783d349226ddb655927e2d4881a67a1e17e68"
+var LANG_CODE = ["de" /* French */, "ar" /* Arabic */, "fr" /* French */, "hi" /* Hindi */, "es" /* Spanish */, "zh" /* Chinese */]
 
 function onRun(context) {
 
@@ -31,33 +33,24 @@ function onRun(context) {
     return
   }
 
-  // get user input
-  var userInput = getUserInput(sketch)
-
-  // get pressed button
-  var userPressedButton = userInput[0] == 1000 ? "Append" : "Cancel"
-
-  // get user text input
-  var userTextInput = userInput[1]
-
-  // check if user pressed append
-  if (userPressedButton == "Append") {
-
-    // call append function
-    doTheThing (selection, numOfLayers, userTextInput)
-
-    // notify if there are any non text layer selected
-    if (nonTextSelected) {
-      document.hideMessage()  // hide previous message
-      document.showMessage("Non-text layer selected, nothing happened to them ^_^")
+  // call append function
+  // doTheThing (selection, numOfLayers, userTextInput)
+  var srcList = [];
+  for (var item = 0; item < numOfLayers; item++) {
+    var layer = selection[item]
+    var layerClass = layer.class()
+    if (layerClass == MSTextLayer) {
+      srcList.push(layer.stringValue())
     }
-
-    // save userInput to use it in appendAgain
-    sketch.setSettingForKey("userTextInput", userTextInput)
   }
+  translateList(srcList, selection, numOfLayers)
 
+  // notify if there are any non text layer selected
+  if (nonTextSelected) {
+    document.hideMessage()  // hide previous message
+    document.showMessage("Non-text layer selected, nothing happened to them ^_^")
+  }
 }
-
 
 // append previous input to current selection
 function appendAgain(context) {
@@ -92,36 +85,371 @@ function appendAgain(context) {
     return
   }
 
-  // append text
-  doTheThing (selection, numOfLayers, previousUserInput)
-}
-
-
-
-// brain function
-function doTheThing (selection, numOfLayers, userTextInput)
-{
-  // check layer class, then append text if its a text layer
-  var item
-  for (item = 0; item < numOfLayers; item++) {
-
-    // get current layer & its class
+  var srcList = [];
+  for (var item = 0; item < numOfLayers; item++) {
     var layer = selection[item]
     var layerClass = layer.class()
-
-    // check layer class if Text layer then append text
     if (layerClass == MSTextLayer) {
-      var newText =  layer.stringValue + userTextInput
-      layer.stringValue = layer.stringValue() + userTextInput
-
-    } else {
-      nonTextSelected = 1
+      srcList.push(layer.stringValue())
     }
   }
 
+  // append text
+  // doTheThing (selection, numOfLayers, previousUserInput)
+  // translateList(srcList, selection, numOfLayers)
+}
+
+function getTranslateUrl(text, lang) {
+  return TRANSLATE_URL + "&text=" + encodeURI(text) + "&lang=" + lang
+}
+
+function translateDe(context) {
+  // "An NSApplication object manages an app’s main event loop in addition to resources used by all of that app’s objects." - Apple
+  var app = [NSApplication sharedApplication]
+
+  // get sketch context
+  var sketch = context.api()
+
+  // get document
+  var document = context.document
+
+  // get selection
+  var selection = context.selection
+
+  // number of layers selected
+  var numOfLayers = selection.count()
+
+  // check if there are no layers selected
+  if (!numOfLayers) {
+    [app displayDialog:"Select me some text layers, Please :/" withTitle:"Appender"]
+    return
+  }
+
+  // check if the user is editing text layer
+  // always layer number 0 will be the one in edit mode
+  var layer = selection[0]
+  if (layer.class() == MSTextLayer && layer.isEditingText()) {
+    [app displayDialog:"I can't append text while you are in edit mode :s" withTitle:"Appender"]
+    return
+  }
+
+  // call append function
+  // doTheThing (selection, numOfLayers, userTextInput)
+  var srcList = [];
+  for (var item = 0; item < numOfLayers; item++) {
+    var layer = selection[item]
+    var layerClass = layer.class()
+    if (layerClass == MSTextLayer) {
+      srcList.push(layer.stringValue())
+    }
+  }
+  
+  translateList(srcList, selection, numOfLayers, 0)
+
+  // notify if there are any non text layer selected
+  if (nonTextSelected) {
+    document.hideMessage()  // hide previous message
+    document.showMessage("Non-text layer selected, nothing happened to them ^_^")
+  }
+}
+
+function translateAr(context) {
+  // "An NSApplication object manages an app’s main event loop in addition to resources used by all of that app’s objects." - Apple
+  var app = [NSApplication sharedApplication]
+
+  // get sketch context
+  var sketch = context.api()
+
+  // get document
+  var document = context.document
+
+  // get selection
+  var selection = context.selection
+
+  // number of layers selected
+  var numOfLayers = selection.count()
+
+  // check if there are no layers selected
+  if (!numOfLayers) {
+    [app displayDialog:"Select me some text layers, Please :/" withTitle:"Appender"]
+    return
+  }
+
+  // check if the user is editing text layer
+  // always layer number 0 will be the one in edit mode
+  var layer = selection[0]
+  if (layer.class() == MSTextLayer && layer.isEditingText()) {
+    [app displayDialog:"I can't append text while you are in edit mode :s" withTitle:"Appender"]
+    return
+  }
+
+  // call append function
+  // doTheThing (selection, numOfLayers, userTextInput)
+  var srcList = [];
+  for (var item = 0; item < numOfLayers; item++) {
+    var layer = selection[item]
+    var layerClass = layer.class()
+    if (layerClass == MSTextLayer) {
+      srcList.push(layer.stringValue())
+    }
+  }
+  
+  translateList(srcList, selection, numOfLayers, 1)
+
+  // notify if there are any non text layer selected
+  if (nonTextSelected) {
+    document.hideMessage()  // hide previous message
+    document.showMessage("Non-text layer selected, nothing happened to them ^_^")
+  }
+}
+
+function translateFr(context) {
+  // "An NSApplication object manages an app’s main event loop in addition to resources used by all of that app’s objects." - Apple
+  var app = [NSApplication sharedApplication]
+
+  // get sketch context
+  var sketch = context.api()
+
+  // get document
+  var document = context.document
+
+  // get selection
+  var selection = context.selection
+
+  // number of layers selected
+  var numOfLayers = selection.count()
+
+  // check if there are no layers selected
+  if (!numOfLayers) {
+    [app displayDialog:"Select me some text layers, Please :/" withTitle:"Appender"]
+    return
+  }
+
+  // check if the user is editing text layer
+  // always layer number 0 will be the one in edit mode
+  var layer = selection[0]
+  if (layer.class() == MSTextLayer && layer.isEditingText()) {
+    [app displayDialog:"I can't append text while you are in edit mode :s" withTitle:"Appender"]
+    return
+  }
+
+  // call append function
+  // doTheThing (selection, numOfLayers, userTextInput)
+  var srcList = [];
+  for (var item = 0; item < numOfLayers; item++) {
+    var layer = selection[item]
+    var layerClass = layer.class()
+    if (layerClass == MSTextLayer) {
+      srcList.push(layer.stringValue())
+    }
+  }
+  
+  translateList(srcList, selection, numOfLayers, 2)
+
+  // notify if there are any non text layer selected
+  if (nonTextSelected) {
+    document.hideMessage()  // hide previous message
+    document.showMessage("Non-text layer selected, nothing happened to them ^_^")
+  }
+}
+
+function translateHi(context) {
+  // "An NSApplication object manages an app’s main event loop in addition to resources used by all of that app’s objects." - Apple
+  var app = [NSApplication sharedApplication]
+
+  // get sketch context
+  var sketch = context.api()
+
+  // get document
+  var document = context.document
+
+  // get selection
+  var selection = context.selection
+
+  // number of layers selected
+  var numOfLayers = selection.count()
+
+  // check if there are no layers selected
+  if (!numOfLayers) {
+    [app displayDialog:"Select me some text layers, Please :/" withTitle:"Appender"]
+    return
+  }
+
+  // check if the user is editing text layer
+  // always layer number 0 will be the one in edit mode
+  var layer = selection[0]
+  if (layer.class() == MSTextLayer && layer.isEditingText()) {
+    [app displayDialog:"I can't append text while you are in edit mode :s" withTitle:"Appender"]
+    return
+  }
+
+  // call append function
+  // doTheThing (selection, numOfLayers, userTextInput)
+  var srcList = [];
+  for (var item = 0; item < numOfLayers; item++) {
+    var layer = selection[item]
+    var layerClass = layer.class()
+    if (layerClass == MSTextLayer) {
+      srcList.push(layer.stringValue())
+    }
+  }
+  
+  translateList(srcList, selection, numOfLayers, 3)
+
+  // notify if there are any non text layer selected
+  if (nonTextSelected) {
+    document.hideMessage()  // hide previous message
+    document.showMessage("Non-text layer selected, nothing happened to them ^_^")
+  }
+}
+
+function translateEs(context) {
+  // "An NSApplication object manages an app’s main event loop in addition to resources used by all of that app’s objects." - Apple
+  var app = [NSApplication sharedApplication]
+
+  // get sketch context
+  var sketch = context.api()
+
+  // get document
+  var document = context.document
+
+  // get selection
+  var selection = context.selection
+
+  // number of layers selected
+  var numOfLayers = selection.count()
+
+  // check if there are no layers selected
+  if (!numOfLayers) {
+    [app displayDialog:"Select me some text layers, Please :/" withTitle:"Appender"]
+    return
+  }
+
+  // check if the user is editing text layer
+  // always layer number 0 will be the one in edit mode
+  var layer = selection[0]
+  if (layer.class() == MSTextLayer && layer.isEditingText()) {
+    [app displayDialog:"I can't append text while you are in edit mode :s" withTitle:"Appender"]
+    return
+  }
+
+  // call append function
+  // doTheThing (selection, numOfLayers, userTextInput)
+  var srcList = [];
+  for (var item = 0; item < numOfLayers; item++) {
+    var layer = selection[item]
+    var layerClass = layer.class()
+    if (layerClass == MSTextLayer) {
+      srcList.push(layer.stringValue())
+    }
+  }
+  
+  translateList(srcList, selection, numOfLayers, 4)
+
+  // notify if there are any non text layer selected
+  if (nonTextSelected) {
+    document.hideMessage()  // hide previous message
+    document.showMessage("Non-text layer selected, nothing happened to them ^_^")
+  }
+}
+
+function translateZh(context) {
+  // "An NSApplication object manages an app’s main event loop in addition to resources used by all of that app’s objects." - Apple
+  var app = [NSApplication sharedApplication]
+
+  // get sketch context
+  var sketch = context.api()
+
+  // get document
+  var document = context.document
+
+  // get selection
+  var selection = context.selection
+
+  // number of layers selected
+  var numOfLayers = selection.count()
+
+  // check if there are no layers selected
+  if (!numOfLayers) {
+    [app displayDialog:"Select me some text layers, Please :/" withTitle:"Appender"]
+    return
+  }
+
+  // check if the user is editing text layer
+  // always layer number 0 will be the one in edit mode
+  var layer = selection[0]
+  if (layer.class() == MSTextLayer && layer.isEditingText()) {
+    [app displayDialog:"I can't append text while you are in edit mode :s" withTitle:"Appender"]
+    return
+  }
+
+  // call append function
+  // doTheThing (selection, numOfLayers, userTextInput)
+  var srcList = [];
+  for (var item = 0; item < numOfLayers; item++) {
+    var layer = selection[item]
+    var layerClass = layer.class()
+    if (layerClass == MSTextLayer) {
+      srcList.push(layer.stringValue())
+    }
+  }
+  
+  translateList(srcList, selection, numOfLayers, 5)
+
+  // notify if there are any non text layer selected
+  if (nonTextSelected) {
+    document.hideMessage()  // hide previous message
+    document.showMessage("Non-text layer selected, nothing happened to them ^_^")
+  }
+}
+
+function translateList(srcList, selection, numOfLayers, langCodeIndex) {
+  var item
+  for (item = 0; item < numOfLayers; item++) {
+    var layer = selection[item]
+    var layerClass = layer.class()
+    if (layerClass == MSTextLayer) {
+      log(LANG_CODE[langCodeIndex])
+      var translateUrl = getTranslateUrl(srcList[item], LANG_CODE[langCodeIndex%6])
+      var text = getJSON(translateUrl)
+      layer.stringValue = "" + text.text
+    }
+  }
+  langCodeIndex++
+}
+
+// Useful function to get json data from url
+function getJSON (url) {
+    // https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20170305T053617Z.246e0c7605e30fc1.f58783d349226ddb655927e2d4881a67a1e17e68&text=settings&lang=de
+    var request = NSURLRequest.requestWithURL(NSURL.URLWithString(url));
+    var response = NSURLConnection.sendSynchronousRequest_returningResponse_error(request, null, null);
+    return JSON.parse(NSString.alloc().initWithData_encoding(response, NSUTF8StringEncoding));
 }
 
 
+function generatePlaceHolder(count) {
+  var result = ""
+  for (var i = 0;i < count; i++) {
+    result += "S"
+  }
+  return result
+}
+
+function generateDummyText(text) {
+  var length = text.length()
+  if (length >=0 && length <= 10) {
+    return generatePlaceHolder(length * 3)
+  } else if (length >= 11 && length <= 20) {
+    return generatePlaceHolder(length * 2)
+  } else if (length >= 21 && length <= 30) {
+    return generatePlaceHolder(length * 1.8)
+  } else if (length >= 31 && length <= 50) {
+    return generatePlaceHolder(length * 1.6)
+  } else if (length >= 51 && length <= 70) {
+    return generatePlaceHolder(length * 1.4)
+  }
+  return generatePlaceHolder(length * 1.3)
+}
 
 // show dialog to get user input
 function getUserInput(sketch){
