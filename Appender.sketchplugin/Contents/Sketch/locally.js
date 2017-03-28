43614996bd6431073c28a278dd24106295da4d26
nsuitter <nsuitter@salesforce.com>
2017-03-28 16:45:18 -0700

    Moving some logic around, limiting functionality to ArtBoardGroup, adding support for multiline pseudolocalization

--- Appender.sketchplugin/Contents/Sketch/locally.js
+++ Appender.sketchplugin/Contents/Sketch/locally.js
@@ -1,6 +1,4 @@
-var nonTextSelected = 0
 var TRANSLATE_URL = "https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20170305T053617Z.246e0c7605e30fc1.f58783d349226ddb655927e2d4881a67a1e17e68"
-var LANG_CODE = ["de" /* French */, "ar" /* Arabic */, "fr" /* French */, "hi" /* Hindi */, "es" /* Spanish */, "zh" /* Chinese */]
 
 function getTranslateUrl(text, lang) {
   return TRANSLATE_URL + "&text=" + encodeURI(text) + "&lang=" + lang
@@ -56,59 +54,55 @@ function translate(context,langCode) {
   var selection = context.selection
 
   // number of layers selected
-  var numOfLayers = selection.count()
+  var numOfLayers = selection.count();
+  if (!numOfLayers) {
+    [app displayDialog:"Please select an Artboard Group to translate text" withTitle:"Locally"]
+     return
+   }
+
+  // always layer number 0 will be the one in edit mode
+  var layer = selection[0]
+
+  // get list of text layers
+  var textLayersFromSelection = layer.children().slice().filter(layer => layer.class() == "MSTextLayer")
+
 
   // check if there are no layers selected
-  if (!numOfLayers) {
-    [app displayDialog:"Select me some text layers, Please :/" withTitle:"Appender"]
+  if (layer.class() != MSArtboardGroup) {
+    [app displayDialog:"You must select an Artboard Group to translate text" withTitle:"Locally"]
     return
   }
 
   // check if the user is editing text layer
-  // always layer number 0 will be the one in edit mode
-  var layer = selection[0]
   if (layer.class() == MSTextLayer && layer.isEditingText()) {
-    [app displayDialog:"I can't append text while you are in edit mode :s" withTitle:"Appender"]
+    [app displayDialog:"I can't append text while you are in edit mode :s" withTitle:"Locally"]
     return
   }
 
-  // call append function
-  // doTheThing (selection, numOfLayers, userTextInput)
-  var srcList = [];
-  for (var item = 0; item < numOfLayers; item++) {
-    var layer = selection[item]
-    var layerClass = layer.class()
-    if (layerClass == MSTextLayer) {
-      srcList.push(layer.stringValue())
-    }
-  }
-  
-  if (langCode == "ps") {
-    pseudoLocalizeList(srcList, selection, numOfLayers)
-  } else {
-    translateList(srcList, selection, numOfLayers, 0)
-  }
-
-  // notify if there are any non text layer selected
-  if (nonTextSelected) {
-    document.hideMessage()  // hide previous message
-    document.showMessage("Non-text layer selected, nothing happened to them ^_^")
-  }
+  translateList(textLayersFromSelection, 0)
 }
 
-function translateList(srcList, selection, numOfLayers, langCodeIndex) {
-  var item
-  for (item = 0; item < numOfLayers; item++) {
-    var layer = selection[item]
-    var layerClass = layer.class()
-    if (layerClass == MSTextLayer) {
-      log(LANG_CODE[langCodeIndex])
-      var translateUrl = getTranslateUrl(srcList[item], LANG_CODE[langCodeIndex%6])
-      var text = getJSON(translateUrl)
-      layer.stringValue = "" + text.text
+function translateList(textLayersFromSelection, langCodeIndex) {
+  for (item = 0; item < textLayersFromSelection.length; item++) {
+    layer = textLayersFromSelection[item];
+    sourceText = layer.stringValue()
+    if (langCode == 'ps') {
+      //handle multiline text layers
+      var textArray = layer.stringValue().split("\n");
+      for (var x = 0; x < textArray.length; x++) {
+          textArray[x] = pseudoLocalize(textArray[x])
+      }
+      var translation = textArray.join("\n")
+      if (translation) {
+          layer.stringValue = translation;
+          layer.adjustFrameToFit()
+      }
+    } else {
+      var translateUrl = getTranslateUrl(sourceText, langCode)
+      var translationJSON = getJSON(translateUrl)
+      layer.stringValue = "" + translationJSON.text
     }
   }
-  langCodeIndex++
 }
 
 // Useful function to get json data from url
@@ -120,9 +114,9 @@ function getJSON (url) {
 }
 
 function pseudoLocalize(string) {
-  var chars = "介引户הаí先ร简аน取정úσιмÜП介мのαşแةбй계ذะяřт紹소иЗ介นşрωtبčنごдčש帳าаůсחое簡üγกвцΕ戶н客иез개介引户הаí先ร简аน取정úσιмÜП介мのαşแةбй계ذะяřт紹소иЗ介นşрωtبčنごдčש帳าаůсחое簡üγกвцΕ戶н客иез개介引户הаí先ร简аน取정úσιмÜП介мのαşแةбй계ذะяřт紹소иЗ介นşрωtبčنごдčש帳าаůсחое簡üγกвцΕ戶н客иез개" 
+  var chars = "介引户הаí 先ร简 аน取정úσι мÜП介мのα şแةбй 계ذะяř т紹소 иЗ介นşрωt بčنごдčש 帳าаůсחо е 簡üγก вцΕ戶н客 иез개介引 户הаí 先ร简аน取정ú σιмÜП 介 мのαşแ ةбй계ذะ яřт紹 소иЗ介นşрω tبčنごд čש帳าаůсחое 簡üγก вцΕ戶 н客иез개 介引户הа í先 ร简аน取정 úσιмÜ П介мのα şแةбй 계ذะяřт紹 소иЗ介น şрω tبčنごд čש帳า аůс ח ое簡üγ กвцΕ戶н客иез 개" 
 
-  var length = string.length();
+  var length = string.length;
   if (length > 100) {
     length = length * 0.3;
   } else if (length > 12) {
@@ -136,17 +130,4 @@ function pseudoLocalize(string) {
     var translation = "[" + string + "]" + chars.slice(0, length) + "]"
   }
   return translation;
-}
-
-function pseudoLocalizeList(srcList, selection, numOfLayers) {
-  var item
-  for (item = 0; item < numOfLayers; item++) {
-    var layer = selection[item]
-    var layerClass = layer.class()
-    if (layerClass == MSTextLayer) {
-      var text = pseudoLocalize(srcList[item])
-      layer.stringValue = "" + text
-      log(text)
-    }
-  }
 }
